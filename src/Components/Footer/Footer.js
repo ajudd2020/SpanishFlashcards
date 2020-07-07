064d81bbbc21c9ac75eace683572d0a298d91c1f
Ana Judd <ajudd1@ufl.edu>
2020-07-06 22:17:42 -0400

    added Footer

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './Footer.css'
+
+
+export default function Footer() {
+    return (
+        <>
+            <div>
+                <div className="footerStyle">Content Provided By <a href="https://www.merriam-webster.com" target ="_blank" rel="noopener noreferrer">Webster's Dictionary</a></div>
+            </div>
+        </>
+    );
+}
