2fabfab3202e6dfd8310dad846134ae05a248a48
Ana Judd <ajudd1@ufl.edu>
2020-07-10 20:23:41 -0400

    Updates

---
+++ precache-manifest.8016856b457ba7f5ce01b7d723818d1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e84c249c3d63c0a3f2fb279fa891fb72",
+    "url": "/SpanishFlashcards/index.html"
+  },
+  {
+    "revision": "68d3e4de7a787a624c33",
+    "url": "/SpanishFlashcards/static/css/main.b11f16a3.chunk.css"
+  },
+  {
+    "revision": "43b8b2fce82c6e87d021",
+    "url": "/SpanishFlashcards/static/js/2.da22c7dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SpanishFlashcards/static/js/2.da22c7dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68d3e4de7a787a624c33",
+    "url": "/SpanishFlashcards/static/js/main.b16d685b.chunk.js"
+  },
+  {
+    "revision": "4e6f9bca4b7a8d5a0bea",
+    "url": "/SpanishFlashcards/static/js/runtime-main.fe772827.js"
+  }
+]);
